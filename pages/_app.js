3ad5f51d13f8b96ab292d6ff54ed960f13ae8736
GrindFit <grindfit.bkk@gmail.com>
2025-08-12 20:24:59 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,5 @@
-// pages/_app.js
-import "../styles/globals.css";
-import Head from "next/head";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* Font exactly as before */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700;800&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
