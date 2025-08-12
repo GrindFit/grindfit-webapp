8b783801773ec0b04098e983cd152ed5c54060db
GrindFit <grindfit.bkk@gmail.com>
2025-08-12 20:20:01 +0700

    Update Brand.jsx

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -1,7 +1,12 @@
-// components/Brand.jsx
 export default function Brand({ className = "" }) {
   return (
-    <span aria-label="GrindFit" title="GrindFit" className={`brand-word ${className}`}>
+    <span
+      className={
+        "inline-block font-extrabold tracking-wider brand-word " + className
+      }
+      aria-label="GrindFit"
+      title="GrindFit"
+    >
       GRINDFIT
     </span>
   );
