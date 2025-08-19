3e2fc4a0ad3828165f0d91548a5e3960b54ad10a
GrindFit <grindfit.bkk@gmail.com>
2025-08-19 17:37:45 +0700

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,7 @@ export default function Nav({ rightSlot }) {
       <div className="w-full flex items-center justify-between h-16 xl:h-20">
         {/* Tiny pad so it doesn't kiss the bezel; set to pl-0 if you want *absolute* flush */}
        <Link href="/" className="no-underline -ml-4 sm:-ml-6">
-  <Brand size="clamp(64px,7.5vw,128px)" />
+ <Brand size="clamp(64px,7.2vw,116px)" />
 </Link>
           {/* Size was good before; keep 2xl. Bump to "3xl" if you want larger. */}
           <Brand size="2xl" />
