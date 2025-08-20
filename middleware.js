6f67f3b642019ac01c49bef7c242063b45a051e8
GrindFit <grindfit.bkk@gmail.com>
2025-08-20 22:05:08 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,7 +3,13 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 
 export async function middleware(req) {
   const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
+const supabase = createMiddlewareClient({
+  req,
+  res,
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+});
+
 
   const {
     data: { session },
