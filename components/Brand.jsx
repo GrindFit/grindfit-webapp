11a3874827c9a8d4498c79e3178e695e297f5a8b
GrindFit <grindfit.bkk@gmail.com>
2025-09-03 15:14:15 +0700

    Update Brand.jsx

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -1,9 +1,7 @@
 // components/Brand.jsx
-export default function Brand({ className = '' }) {
+export default function Brand() {
   return (
-    <span
-      className={`gf-gradient-text font-extrabold tracking-tight text-[22px] sm:text-[24px] ${className}`}
-    >
+    <span className="gf-gradient-text font-black tracking-tight text-[22px] sm:text-[24px]">
       GRINDFIT
     </span>
   );
